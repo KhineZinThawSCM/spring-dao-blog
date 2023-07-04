2cb85f2f18c849d4cb126aa906ef6be1fd09e4b4
Khine Zin Thaw (SCM) <help>
2023-07-04 14:35:21 +0630

    remove unused package

--- src/main/java/springblog/web/form/ResetPasswordForm.java
+++ src/main/java/springblog/web/form/ResetPasswordForm.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import springblog.common.validation.ConfirmPassword;
 
 @Getter
 @Setter
